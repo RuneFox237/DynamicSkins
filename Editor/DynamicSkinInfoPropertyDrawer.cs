7cf5c7ef8458df625d09b84e69c279c730b089a6
RuneFox237 <kambal_mo@me.com>
2022-04-22 01:10:05 -0700

    Removed code for alternate package locations
    I thought I needed different versions of the copy command for different locations. turns out Unity has their own handling and Package/... just works for both.

--- Editor/DynamicSkinInfoPropertyDrawer.cs
+++ Editor/DynamicSkinInfoPropertyDrawer.cs
@@ -21,35 +21,39 @@ namespace DynamicSkinBuilder.CustomEditors
     [MenuItem("Assets/Create/DynamicSkins/Create DynamicBones.asmdef",false,1)]
     static void TryMove()
     {
-      bool TryZip = false;
-      bool TryManaged = false;
-
       //Debug.Log(AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID()));
       var path = AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID());
-      try //if DynamicSkins was added from zip
-      {
-        File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/DynamicBones~/DynamicBones.asmdef", path + "/DynamicBones.asmdef", true);
-        File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/DynamicBones~/DynamicBones.asmdef.meta", path + "/DynamicBones.asmdef.meta", true);
-      }
-      catch
-      {
-        TryZip = true;//An exception has occured
-      }
 
-      try //if DynamicSkins was added from package manager
-      {
-        //File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/DynamicBones~/DynamicBones.asmdef", AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID()) + "/DynamicSkins.asmdef", true);
-        //File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/DynamicBones~/DynamicBones.asmdef.meta", AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID()) + "/DynamicSkins.asmdef.meta", true);
-      }
-      catch
-      {
-        TryManaged = true;//An exception has occured
-      }
+      //bool TryZip = false;
+      //bool TryManaged = false;
+      //
+      //try //if DynamicSkins was added from zip
+      //{
+      //  File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/DynamicBones~/DynamicBones.asmdef", path + "/DynamicBones.asmdef", true);
+      //  File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/DynamicBones~/DynamicBones.asmdef.meta", path + "/DynamicBones.asmdef.meta", true);
+      //}
+      //catch
+      //{
+      //  TryZip = true;//An exception has occured
+      //}
+      //
+      //try //if DynamicSkins was added from package manager
+      //{
+      //  File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/DynamicBones~/DynamicBones.asmdef", AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID()) + "/DynamicSkins.asmdef", true);
+      //  File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/DynamicBones~/DynamicBones.asmdef.meta", AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID()) + "/DynamicSkins.asmdef.meta", true);
+      //}
+      //catch
+      //{
+      //  TryManaged = true;//An exception has occured
+      //}
+      //
+      //if (TryManaged && TryZip)
+      //{
+      //  Debug.LogError("Error when creating DynamicBones.asmdef");
+      //}
 
-      if (TryManaged && TryZip)
-      {
-        Debug.LogError("Error when creating DynamicBones.asmdef");
-      }
+      File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/DynamicBones~/DynamicBones.asmdef", path + "/DynamicBones.asmdef", true);
+      File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/DynamicBones~/DynamicBones.asmdef.meta", path + "/DynamicBones.asmdef.meta", true);
 
 
       AssetDatabase.StartAssetEditing();
@@ -89,39 +93,42 @@ namespace DynamicSkinBuilder.CustomEditors
       var DynamicSkinCode = new DynamicSkinTemplate(dynamicSkinInfo);
       File.WriteAllText(path, DynamicSkinCode.TransformText());
 
-      bool TryZip = false;
-      bool TryManaged = false;
+      //bool TryZip = false;
+      //bool TryManaged = false;
+      //
+      //try //if DynamicSkins was added from zip
+      //{
+      //  File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/CopyPath/DynamicSkinHelpers.cs", dynamicSkinInfo.assetInfo.modFolder + "/DynamicSkinHelpers.cs", true);
+      //  File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/CopyPath/RuneFox_Utils.cs", dynamicSkinInfo.assetInfo.modFolder + "/RuneFox_Utils.cs", true);
+      //}
+      //catch
+      //{
+      //  TryZip = true;//An exception has occured
+      //}
+      //
+      //try //if DynamicSkins was added from package manager
+      //{
+      //  //File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/CopyPath/DynamicSkinHelpers.cs", dynamicSkinInfo.assetInfo.modFolder + "/DynamicSkinHelpers.cs", true);
+      //  //File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/CopyPath/RuneFox_Utils.cs", dynamicSkinInfo.assetInfo.modFolder + "/RuneFox_Utils.cs", true);
+      //}
+      //catch
+      //{
+      //  TryManaged = true;//An exception has occured
+      //}
+      //
+      //if (TryManaged && TryZip)
+      //{
+      //  Debug.LogError("Dynamic Skins has errored on copying files to the Mod Folder: " + dynamicSkinInfo.assetInfo.modFolder);
+      //}
 
-      try //if DynamicSkins was added from zip
-      {
-        File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/CopyPath/DynamicSkinHelpers.cs", dynamicSkinInfo.assetInfo.modFolder + "/DynamicSkinHelpers.cs", true);
-        File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/CopyPath/RuneFox_Utils.cs", dynamicSkinInfo.assetInfo.modFolder + "/RuneFox_Utils.cs", true);
-      }
-      catch
-      {
-        TryZip = true;//An exception has occured
-      }
-
-      try //if DynamicSkins was added from package manager
-      {
-        File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/CopyPath/DynamicSkinHelpers.cs", dynamicSkinInfo.assetInfo.modFolder + "/DynamicSkinHelpers.cs", true);
-        File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/CopyPath/RuneFox_Utils.cs", dynamicSkinInfo.assetInfo.modFolder + "/RuneFox_Utils.cs", true);
-      }
-      catch
-      {
-        TryManaged = true;//An exception has occured
-      }
-
-      if (TryManaged && TryZip)
-      {
-        Debug.LogError("Dynamic Skins has errored on copying files to the Mod Folder: " + dynamicSkinInfo.assetInfo.modFolder);
-      }
+      //turns out that this still works the same even if the package is in the package cache
+      File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/CopyPath/DynamicSkinHelpers.cs", dynamicSkinInfo.assetInfo.modFolder + "/DynamicSkinHelpers.cs", true);
+      File.Copy("Packages/com.runefox237.dynamicskinbuilder/Editor/CopyPath/RuneFox_Utils.cs", dynamicSkinInfo.assetInfo.modFolder + "/RuneFox_Utils.cs", true);
 
       AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
       AssetDatabase.ImportAsset(dynamicSkinInfo.assetInfo.modFolder + "/DynamicSkinHelpers.cs", ImportAssetOptions.ForceUpdate);
       AssetDatabase.ImportAsset(dynamicSkinInfo.assetInfo.modFolder + "/RuneFox_Utils.cs", ImportAssetOptions.ForceUpdate);
 
-      //var HelperCode = new 
 
       foreach (var mod in dynamicSkinInfo.DynamicModifications)
       {
